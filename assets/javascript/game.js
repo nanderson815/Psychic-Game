1b8c9165034e67a7f47b0ef1959dda3811095314
Noah A <nanderson815@gmail.com>
2018-11-30 09:15:56 -0500

    Updated 11 30 18

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+
+var userGuesses = document.getElementById("userGusses"); 
+var guessesCounter = document.getElementById("guessesCounter");
+var winCounter = document.getElementById("winCounter");
+var lossesCounter = document.getElementById("lossesCounter");
+
+document.onkeyup = function (event){
+
+
+}
