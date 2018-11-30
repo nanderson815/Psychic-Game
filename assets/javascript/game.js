3f2a59d101b3d9e9c7fd9e097a8b8c91eb21fba2
Noah A <nanderson815@gmail.com>
2018-11-30 11:54:43 -0500

    Updated 11 30 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,14 @@ var computerGuess = "";
 // Array to hold this list of guesses.
 var guessList = [];
 
-var score = 0;
+// Variable to count wins
+var winScore = 0;
+
+// Variable to count losses.
+var lossScore = 0;
+
+// Variable that counts remaining guesses
+var guessesRemaining = 9;
 
 // variables that link to HTML Ids.
 var userGuesses = document.getElementById("userGuesses");
@@ -14,24 +21,47 @@ var guessesCounter = document.getElementById("guessesCounter");
 var winCounter = document.getElementById("winCounter");
 var lossesCounter = document.getElementById("lossesCounter");
 
-document.onkeyup = function (event) {
-    
+document.onkeydown = function (event) {
+
     // Generates a random letter - "computerGuess"
     computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(computerGuess);
 
-    // Generates up to 6 user guesses
-    if (guessList.length < 6) {
+    // Counts down remaining guesses.
+    guessesRemaining -= 1;
+    guessesCounter.innerHTML = guessesRemaining;
+
+    // Logs up to 9 user guesses
+    if (guessList.length < 9) {
+        // sets guess var to user input.
         var guess = event.key;
-        if (guess == computerGuess){
+        // if guess is correct, alert user, add 1 to wins, reset guess list, resets guess counter.
+        if (guess == computerGuess) {
             alert("The answer was " + guess + ", you win!");
-            score += 1;
-            winCounter.innerHTML = score;
+            winScore += 1;
+            winCounter.innerHTML = winScore;
             guessList = [];
             userGuesses.innerHTML = guessList;
+            guessesRemaining = 9;
+            guessesCounter.innerHTML = 9;
+            // If guesses are incorret, display each incorrect guess.
         } else {
-        guessList.push(guess);
-        userGuesses.innerHTML = guessList;
+            guessList.push(guess);
+            userGuesses.innerHTML = guessList;
         }
+    }
+
+}
+// Once guesses run out - alert user, empty guessList, increase loss score by 1, resets guess counter.
+// Function uses onkeyup so no 7th keypress is required.
+document.onkeyup = function (event) {
+    if (guessList.length == 9) {
+        alert("You lose!");
+        guessList = [];
+        userGuesses.innerHTML = guessList;
+        lossScore += 1;
+        lossesCounter.innerHTML = lossScore;
+        guessesRemaining = 9;
+        guessesCounter.innerHTML = 9;
     }
 }
